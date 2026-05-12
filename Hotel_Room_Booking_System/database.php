42eac9d5aa27a3f22ba0ce9c227d12b8bfc2e02a
munifmubtashim <munifmubtasim@gmail.com>
2026-05-12 20:40:14 +0600

    Add database connection class

---
+++ Hotel_Room_Booking_System/database.php
@@ -0,0 +1,19 @@
+<?php
+class db {
+function connection() {
+    $db_host     = "localhost";
+    $db_user     = "root";
+    $db_password = "";
+    $db_name     = "hotel_booking";
+
+    $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+    if ($connection->connect_error) {
+        die("Could not Connect Database: " . $connection->connect_error);
+    }
+
+    return $connection;
+}
+}
+
+?>
